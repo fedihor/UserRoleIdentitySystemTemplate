e75e7ab9543921a5179bc11610847e40abce1d2b
fedihor <fedihor@ukr.net>
2018-02-04 14:45:14 +0300

    Add identity packages

---
+++ Ite/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Ite.Models
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext() : base("DefaultConnection") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
